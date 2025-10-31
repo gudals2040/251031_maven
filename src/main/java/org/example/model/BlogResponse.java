89132e3e36095cca45bd5630abe23b2aec604d88
PARK HYEONG MIN <qd0420@gmail.com>
2025-10-31 11:32:36 +0900

    step02 끝

---
+++ src/main/java/org/example/model/BlogResponse.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import java.util.List;
+
+// public 1개만 존재할 수 있음
+public record BlogResponse(
+        List<BlogItem> items
+) {
+    public record BlogItem(
+            String title,
+            String link,
+            String description,
+            String bloggername,
+            String bloggerlink,
+            String postdate
+    ) {}
+}
